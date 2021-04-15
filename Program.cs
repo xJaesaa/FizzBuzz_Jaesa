e6264393b6617ad3424c3fe659b63121eff797e4
xJaesaa <103669129@student.swin.edu.au>
2021-04-15 13:52:52 +1000

    fizz buzz

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++){ //as long as i is greater than or equal to 100 it will continue until it does
+                if(i % 3 == 0 && i % 5 == 0){ //
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else{
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
